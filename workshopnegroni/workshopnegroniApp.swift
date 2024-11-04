7ae5762aa83745914505c39152db122740b4e4cd
federicasgammato <fsgammato24@fed.idserve.net>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ workshopnegroni/workshopnegroniApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshopnegroniApp.swift
+//  workshopnegroni
+//
+//  Created by Federica Sgammato on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct workshopnegroniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
